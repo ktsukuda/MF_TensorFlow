a454951da392d1514396156aef9951cb5af2c674
ktsukuda <k.tsukuda@aist.go.jp>
2020-07-30 15:59:56 +0900

    [add] save train result

--- MF_TensorFlow/main.py
+++ MF_TensorFlow/main.py
@@ -1,3 +1,5 @@
+import os
+import json
 import configparser
 import numpy as np
 import tensorflow.compat.v1 as tf
@@ -8,7 +10,9 @@ import evaluation
 from MF import MF
 
 
-def train(model, data_splitter, validation_data, batch_size, config):
+def train(result_dir, model, data_splitter, validation_data, batch_size, config):
+    epoch_data = []
+    best_ndcg = 0
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch in range(config.getint('MODEL', 'epoch')):
@@ -22,7 +26,11 @@ def train(model, data_splitter, validation_data, batch_size, config):
                 start += batch_size
                 total_loss += loss
             hit_ratio, ndcg = evaluation.evaluate(model, sess, validation_data, config.getint('EVALUATION', 'top_k'))
+            epoch_data.append({'epoch': epoch, 'loss': total_loss, 'HR': hit_ratio, 'NDCG': ndcg})
+            if ndcg > best_ndcg:
+                tf.train.Saver().save(sess, os.path.join(result_dir, 'model'))
             print('[Epoch {}] Loss = {:.2f}, HR = {:.4f}, NDCG = {:.4f}'.format(epoch, total_loss, hit_ratio, ndcg))
+    return epoch_data
 
 
 def get_feed_dict(model, train_data, start, end):
@@ -31,6 +39,11 @@ def get_feed_dict(model, train_data, start, end):
     feed_dict[model.item_ids] = train_data[start:end, 1]
     feed_dict[model.ratings] = train_data[start:end, 2]
     return feed_dict
+
+
+def save_train_result(result_dir, epoch_data):
+    with open(os.path.join(result_dir, 'epoch_data.json'), 'w') as f:
+        json.dump(epoch_data, f, indent=4)
 
 
 def main():
@@ -47,8 +60,12 @@ def main():
                 for l2_weight in map(float, config['MODEL']['l2_weight'].split()):
                     print('batch_size = {}, lr = {}, latent_dim = {}, l2_weight = {}'.format(
                         batch_size, lr, latent_dim, l2_weight))
+                    result_dir = "data/train_result/batch_size_{}-lr_{}-latent_dim_{}-l2_weight_{}-epoch_{}-n_negative_{}-top_k_{}".format(
+                        batch_size, lr, latent_dim, l2_weight, config['MODEL']['epoch'], config['MODEL']['n_negative'], config['EVALUATION']['top_k'])
+                    os.makedirs(result_dir, exist_ok=True)
                     model = MF(data_splitter.n_user, data_splitter.n_item, lr, latent_dim, l2_weight)
-                    train(model, data_splitter, validation_data, batch_size, config)
+                    epoch_data = train(result_dir, model, data_splitter, validation_data, batch_size, config)
+                    save_train_result(result_dir, epoch_data)
 
 
 if __name__ == "__main__":
